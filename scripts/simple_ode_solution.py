5cfe5a459d5e149ea114cc3d7b8ef9c16d80c3dc
Niall Oswald <niall.oswald@hotmail.co.uk>
2022-06-01 12:08:58 +0100

    Create a simple ode test script

---
+++ scripts/simple_ode_solution.py
@@ -0,0 +1,18 @@
+from finite_diff.finite_diff import Interpolation
+import numpy as np
+import matplotlib.pyplot as plt
+
+# N = 201, q = 12
+n = 201
+q = 12
+
+inter = Interpolation(n, q)
+x = inter.inter.x
+
+A = np.concatenate(
+    [[np.eye(n + 1)[0, :]], np.array([inter.nderivative(u, 1) for u in x[1:]])]
+)
+b = np.concatenate([[-1 / 3], x[1:] ** 2])
+
+plt.plot(x, np.dot(np.linalg.inv(A), b))
+plt.show()
