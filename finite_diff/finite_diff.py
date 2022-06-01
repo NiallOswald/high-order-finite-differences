ddfaf4ab7e8957bb4f15a3e08583cc698f4b5244
Niall Oswald <niall.oswald@hotmail.co.uk>
2022-06-01 18:07:32 +0100

    Mass refactor for efficiency benefits

--- finite_diff/finite_diff.py
+++ finite_diff/finite_diff.py
@@ -9,7 +9,7 @@ class ConvergenceError(RuntimeError):
     pass
 
 
-class BaseLagrange:
+class Lagrange:
     """A Base class for Lagrange and PiFactor."""
 
     def __init__(self, x):
@@ -19,7 +19,7 @@ class BaseLagrange:
     def __call__(self, y, i):
         index_slice = np.ones(self.n, dtype=bool)
         index_slice[i] = False
-        return np.prod(y - self.x[index_slice])
+        return np.prod(y - self.x[index_slice]) / self._denominator(i)
 
     def nderivative(self, y, i, k):
         counters = np.concatenate(([i], np.zeros(k, dtype=int)), dtype=int)
@@ -44,17 +44,7 @@ class BaseLagrange:
             index -= 1
             counters[index] += 1
 
-        return total
-
-
-class Lagrange(BaseLagrange):
-    """A general Lagrange polynomial for a set of points x."""
-
-    def __call__(self, y, i):
-        return super()(y, i) / self._denominator(i)
-
-    def nderivative(self, y, i, k):
-        return super().nderivative(y, i, k) / self._denominator(i)
+        return total / self._denominator(i)
 
     def _denominator(self, i):
         index_slice = np.ones(self.n, dtype=bool)
@@ -109,86 +99,85 @@ class Stencil:
         return iter(self.stencil)
 
 
-class PiFactor:
-    def __init__(self, x, q, s):
-        self.x = x
-        self.q = q
-        self.s = s
-
-    def __call__(self, y):
-        return np.prod([y - self.x[self.s + k] for k in range(self.q + 1)])
-
-    def derivative(self, y):
-        """Return the derivative of the pi factor at y."""
-        return np.sum(
-            [
-                np.prod(
-                    [
-                        y - self.x[self.s + i]
-                        for i in range(self.q + 1)
-                        if i != k
-                    ]
-                )
-                for k in range(self.q + 1)
-            ]
-        )
-
-    def second_derivative(self, y):
-        """Return the second derivative of the pi factor at y."""
-        return 2 * np.sum(
-            [
-                np.sum(
-                    [
-                        np.prod(
-                            [
-                                y - self.x[self.s + i]
-                                for i in range(self.q + 1)
-                                if i != j and i != k
-                            ]
-                        )
-                        for j in range(k + 1, self.q + 1)
-                    ]
-                )
-                for k in range(self.q + 1)
-            ]
-        )
-
-
 class PolyFactor:
-    """Factors appearing in the error of the interpolant."""
-
     def __init__(self, x, q):
         self.x = x
         self.q = q
-        self._build_factors()
+        self.n = len(x) - 1
 
-    def _build_factors(self):
-        n = len(self.x) - 1
-        s = 0
-        self.poly_factors = []
-        for i in range(n + 1):
-            if i > self.q // 2 and s < n - self.q:
-                s += 1
+        self.first_slicer = self._first_slicer(self.q + 1)
+        self.second_slicer = self._second_slicer(self.q + 1)
 
-            self.poly_factors.append(PiFactor(self.x, self.q, s))
+    def __call__(self, y, i):
+        s = self._find_s(i)
+        return np.prod([y - self.x[s + k] for k in range(self.q + 1)])
+
+    def derivative(self, y, i):
+        s = self._find_s(i)
+        u = np.tile(self.x[s : s + self.q + 1], (self.q + 1, 1))
+        return np.sum(
+            np.prod(
+                y - u[self.first_slicer].reshape((self.q + 1, self.q)), axis=1
+            )
+        )
+
+    def second_derivative(self, y, i):
+        s = self._find_s(i)
+        u = np.tile(self.x[s : s + self.q + 1], (self.q * (self.q + 1), 1))
+        return np.sum(
+            np.prod(
+                y
+                - u[self.second_slicer].reshape(
+                    (self.q * (self.q + 1), self.q - 1)
+                ),
+                axis=1,
+            )
+        )
 
     def __len__(self):
-        return len(self.poly_factors)
+        return self.n + 1
 
-    def __getitem__(self, i):
-        return self.poly_factors[i]
+    def _find_s(self, i):
+        if i == -1:  # Bit of a bodge, should fix
+            return self.n - self.q
 
-    def __iter__(self):
-        return iter(self.poly_factors)
+        if i < self.q // 2:
+            return 0
+        elif i >= self.q // 2 and i < self.n - self.q // 2:
+            return i - self.q // 2
+        else:
+            return self.n - self.q
+
+    @staticmethod
+    def _first_slicer(n):
+        index_slice = np.ones((n, n), dtype=int) - np.eye(n, dtype=bool)
+
+        return index_slice.astype(bool)
+
+    @staticmethod
+    def _second_slicer(n):
+        index_slice = np.ones((n, n, n), dtype=int)
+        slicer = np.tile(np.eye(n, dtype=bool), (n, 1, 1))
+        index_slice -= slicer
+        index_slice -= np.transpose(slicer, axes=(1, 0, 2))
+        index_slice = index_slice.reshape((n * n, n))
+        invalid = np.arange(n)
+        invalid += invalid * n
+        index_slice = np.delete(index_slice, invalid, axis=0)
+
+        return index_slice.astype(bool)
 
 
 class Interpolation:
     """A final interpolation of the unknown function at x."""
 
-    def __init__(self, n, q, tol=1e-5, max_iter=2500):
+    def __init__(self, n, q, tol=1e-6, max_iter=2500):
         self.n = n
         self.q = q
         self.endpoints, self.inter = self._find_endpoints(tol, max_iter)
+
+    def __getitem__(self, i):
+        return self.inter[i]
 
     def _find_endpoints(self, tol, max_iter):
         endpoints = np.linspace(-1, 1, self.n + 2)
@@ -205,20 +194,26 @@ class Interpolation:
 
             h = k
 
-            for i, (f1, f2, e1, e2) in self._generate_iter(factors, extrema):
+            for i in range(len(extrema) - 1):
                 if i == len(factors) - 2:
                     continue
-                if abs(f1(e1)) > abs(f2(e2)):
-                    endpoints[i + 2] -= (endpoints[i + 2] - e1) / (2 * h)
+                if abs(factors(extrema[i], i)) > abs(
+                    factors(extrema[i + 1], i + 1)
+                ):
+                    endpoints[i + 2] -= (endpoints[i + 2] - extrema[i]) / (
+                        2 * h
+                    )
                 else:
-                    endpoints[i + 2] += (e2 - endpoints[i + 2]) / (2 * h)
+                    endpoints[i + 2] += (extrema[i + 1] - endpoints[i + 2]) / (
+                        2 * h
+                    )
 
-            if abs(factors[0](-1)) > abs(factors[0](extrema[0])):
+            if abs(factors(-1, 0)) > abs(factors(extrema[0], 0)):
                 endpoints[1] -= (endpoints[1] + 1) / (2 * h)
             else:
                 endpoints[1] += (extrema[0] - endpoints[1]) / (2 * h)
 
-            if abs(factors[-1](1)) < abs(factors[-1](extrema[-1])):
+            if abs(factors(1, -1)) < abs(factors(extrema[-1], -1)):
                 endpoints[-2] -= (1 - endpoints[-2]) / (2 * h)
             else:
                 endpoints[-2] += (endpoints[-1] - endpoints[-2]) / (2 * h)
@@ -239,38 +234,27 @@ class Interpolation:
             [
                 newton_raphson(
                     (endpoints[i] + endpoints[i + 1]) / 2,
-                    poly_factors[i].derivative,
-                    poly_factors[i].second_derivative,
+                    poly_factors.derivative,
+                    poly_factors.second_derivative,
+                    i=i,
                 )
                 for i in range(len(endpoints) - 2)
             ]
         )
 
-    def _generate_iter(self, factors, extrema):
-        return enumerate(
-            zip(
-                factors.poly_factors[:-1],
-                factors.poly_factors[1:],
-                extrema[:-1],
-                extrema[1:],
-            )
-        )
-
     def _extrema_diff(self, factors, extrema):
         """Find the sum of the adjacent extrema differences."""
-        iterator = self._generate_iter(factors, extrema)
-
         return (
             sum(
-                abs(abs(f1(e1)) - abs(f2(e2)))
-                for i, (f1, f2, e1, e2) in iterator
+                abs(
+                    abs(factors(extrema[i], i))
+                    - abs(factors(extrema[i + 1], i + 1))
+                )
+                for i in range(len(extrema) - 1)
             )
-            + abs(abs(factors[0](-1)) - abs(factors[0](extrema[0])))
-            + abs(abs(factors[-1](extrema[-1])) - abs(factors[-1](1)))
+            + abs(abs(factors(-1, 0)) - abs(factors(extrema[0], 0)))
+            + abs(abs(factors(extrema[-1], -1)) - abs(factors(1, -1)))
         )
-
-    def __getitem__(self, i):
-        return self.inter[i]
 
     def _find_domain(self, y):
         if y < self.endpoints[0] or y > self.endpoints[-1]:
@@ -296,11 +280,11 @@ class Interpolation:
         return self._spacing(p, p.nderivative(y, k))
 
 
-def newton_raphson(x, f, df, tol=1e-8):
+def newton_raphson(x, f, df, tol=1e-8, **kwargs):
     """Find the root of f(x) = 0 using Newton-Raphson."""
     x0 = x
     while True:
-        x1 = x0 - f(x0) / df(x0)
+        x1 = x0 - f(x0, **kwargs) / df(x0, **kwargs)
         if abs(x1 - x0) < tol:
             return x1
         x0 = x1
