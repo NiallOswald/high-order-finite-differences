6ed5b4a9883beca27544169bd3bcad382b490108
Niall Oswald <niall.oswald@hotmail.co.uk>
2022-06-07 22:33:02 +0100

    Implement script for tuning non-uniform spacing

---
+++ scripts/spacing.py
@@ -0,0 +1,45 @@
+from finite_diff.finite_diff import Interpolation, PolyFactor
+import numpy as np
+import matplotlib.pyplot as plt
+
+n = 500
+iters = [1000]  # , 1000, 1500, 2000, 2500]
+
+colour_grid = np.linspace(1, 0, len(iters))
+cmap = plt.get_cmap("viridis")
+
+for k, c in zip(iters, colour_grid):
+    p = Interpolation(n, 4, max_iter=k)
+    x = p.inter.x
+    extrema = x[1:-1]
+
+    factors = PolyFactor(p.endpoints[1:-1], p.q - 1)
+
+    errors = np.concatenate(
+        [
+            [abs(abs(factors(-1, 0)) - abs(factors(extrema[0], 0)))],
+            [
+                abs(
+                    abs(factors(extrema[i], i))
+                    - abs(factors(extrema[i + 1], i + 1))
+                )
+                for i in range(len(extrema) - 1)
+            ],
+            [abs(abs(factors(extrema[-1], -1)) - abs(factors(1, -1)))],
+        ]
+    )
+
+    maxima = np.concatenate(
+        [
+            [abs(factors(-1, 0))],
+            [abs(factors(extrema[i], i)) for i in range(len(extrema))],
+            [abs(factors(1, -1))],
+        ]
+    )
+
+    # print(errors)
+    print(maxima)
+
+    plt.plot(np.concatenate([[-1], extrema, [1]]), maxima, c=cmap(c))
+
+plt.show()
