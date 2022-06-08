c4d5acadc22d373cb0c20b176db1fd87a1dfadc3
Niall Oswald <niall.oswald@hotmail.co.uk>
2022-06-08 17:03:34 +0100

    Update convergence to add plots depending on q

--- scripts/convergence.py
+++ scripts/convergence.py
@@ -4,9 +4,10 @@ from finite_diff.finite_diff import Interpolation
 import numpy as np
 import matplotlib.pyplot as plt
 
-n_vals = [20, 40, 80, 160, 320, 640]
-q_vals = [1, 2, 4, 8, 12, 16, 20]
-eps = 1e-3
+n_vals = [20, 30, 40, 50, 60, 70, 80, 90, 100]
+q_vals = [2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+cmap = plt.get_cmap("viridis")
 
 # True solution
 u = (
@@ -20,11 +21,13 @@ u_true = np.vectorize(u)
 x_n = []
 u_n = []
 
-colour_grid = np.linspace(1, 0, len(n_vals))
-cmap = plt.get_cmap("viridis")
+eps = 1e-3
+q = 4
 
-for n, i in zip(n_vals, colour_grid):
-    inter = Interpolation(n, q=4, boundary=(0, 1), max_iter=500)
+colour_grid_n = np.linspace(1, 0, len(n_vals))
+
+for n, i in zip(n_vals, colour_grid_n):
+    inter = Interpolation(n, q, boundary=(0, 1), max_iter=500)
     x = inter.inter.x
     x_n.append(x)
 
@@ -41,7 +44,7 @@ for n, i in zip(n_vals, colour_grid):
     u = np.linalg.solve(L, b)
     u_n.append(u)
 
-    plt.plot(x, u, c=cmap(i), label=f"$n = {n}$")
+    plt.plot(x, u, c=cmap(i), label=f"$N = {n}$")
 
 plt.xlabel("$x$")
 plt.ylabel("$u(x)$")
@@ -50,10 +53,54 @@ plt.show()
 
 # Error plots
 errors = [abs(u_n[i] - u_true(x_n[i])) for i in range(len(n_vals))]
-for i, j in zip(range(len(n_vals)), colour_grid):
-    plt.plot(x_n[i], errors[i], c=cmap(j), label=f"$n = {n_vals[i]}$")
+for i, j in zip(range(len(n_vals)), colour_grid_n):
+    plt.plot(x_n[i], errors[i], c=cmap(j), label=f"$N = {n_vals[i]}$")
 plt.xlabel("$x$")
 plt.ylabel("Error: $|u(x) - u_i(x)|$")
 plt.yscale("log")
+plt.legend()
+plt.show()
+
+# Convergence over q, n fixed
+x_q = []
+u_q = []
+
+eps = 1e-2
+n = 250
+
+colour_grid_q = np.linspace(1, 0, len(q_vals))
+
+for q, i in zip(q_vals, colour_grid_q):
+    inter = Interpolation(n, q, boundary=(0, 1), max_iter=500)
+    x = inter.inter.x
+    x_q.append(x)
+
+    b = 1 + 2 * x
+    b[0] = 0
+    b[n] = 1
+
+    A = np.array([inter.nderivative(u, 1) for u in x])
+    B = np.array([inter.nderivative(u, 2) for u in x])
+
+    L = eps * B + A
+    L[0, :] = np.eye(n + 1)[0, :]
+    L[n, :] = np.eye(n + 1)[n, :]
+    u = np.linalg.solve(L, b)
+    u_q.append(u)
+
+    plt.plot(x, u, c=cmap(i), label=f"$q = {q}$")
+
+plt.xlabel("$x$")
+plt.ylabel("$u(x)$")
+plt.legend()
+plt.show()
+
+# Error plots
+errors = [abs(u_q[i] - u_true(x_q[i])) for i in range(len(q_vals))]
+for i, j in zip(range(len(q_vals)), colour_grid_q):
+    plt.plot(x_q[i], errors[i], c=cmap(j), label=f"$q = {q_vals[i]}$")
+plt.xlabel("$x$")
+plt.ylabel("Error: $|u(x) - u_i(x)|$")
+# plt.yscale("log")
 plt.legend()
 plt.show()
