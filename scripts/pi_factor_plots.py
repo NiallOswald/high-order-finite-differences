9434b9da2263ae465ba34466c3256fcdc04785c6
Niall Oswald <niall.oswald@hotmail.co.uk>
2022-06-16 23:07:05 +0100

    Partial fix pi_factor_plots.py

--- scripts/pi_factor_plots.py
+++ scripts/pi_factor_plots.py
@@ -2,6 +2,8 @@ from finite_diff.finite_diff import PolyFactor, Interpolation
 import numpy as np
 import matplotlib.pyplot as plt
 
+plt.rcParams.update({"font.size": 11})
+
 # Uniform Grid N = 10 fig 2. a)
 y = np.linspace(-1, 1, 11)
 x = [np.linspace(s, e, 101) for s, e in zip(y[:-1], y[1:])]
@@ -12,17 +14,38 @@ u = [abs(np.vectorize(p, excluded=["i"])(x[i], i)) for i in range(len(x))]
 for a, b in zip(x, u):
     plt.plot(a, b, color="black")
 plt.yscale("log")
+plt.xlabel("$x$", fontsize=16)
+plt.ylabel("$|\\pi(x)|$", fontsize=16)
 plt.ylim(1e-5, 1e-2)
 plt.show()
 
+# Chebshev Roots N = 10 fig 2. b)
+y = 2 * np.arange(1, 11) - 1
+y = -np.cos(y * np.pi / (2 * 10))
+# y = np.concatenate([[-1], y, [1]])
+
+x = [np.linspace(s, e, 1001) for s, e in zip(y[:-1], y[1:])]
+
+p = PolyFactor(y, 9)
+u = [abs(np.vectorize(p, excluded=["i"])(x[i], i)) for i in range(len(x))]
+
+for a, b in zip(x, u):
+    plt.plot(a, b, color="black")
+plt.yscale("log")
+plt.xlabel("$x$", fontsize=16)
+plt.ylabel("$|\\pi(x)|$", fontsize=16)
+plt.ylim(1e-5, 1e-2)
+plt.show()
+raise Exception("Stop")
+
 # Proposed Grid N = 10, q = 6 fig 3. a)
-p = Interpolation(10, 6)
-a = p.inter.x
-f = PolyFactor(a, 6)
-y = p.endpoints
+f = Interpolation(10, 6)
+a = f.inter.x
+p = PolyFactor(a, 6)
+y = f.endpoints
 
 x = [np.linspace(s, e, 101) for s, e in zip(y[:-1], y[1:])]
-u = [abs(np.vectorize(g)(z)) for g, z in zip(f, x)]
+u = [abs(np.vectorize(p, excluded=["i"])(x[i], i)) for i in range(len(x))]
 
 for w, z in zip(x, u):
     plt.plot(w, z, color="black")
