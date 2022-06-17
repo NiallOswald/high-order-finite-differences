3e3b218f3ad2bc6adcf78f3f8fb2bc700a058423
Niall Oswald <niall.oswald@hotmail.co.uk>
2022-06-17 11:53:27 +0100

    Update label font sizes

--- scripts/runge_interpolation.py
+++ scripts/runge_interpolation.py
@@ -4,10 +4,12 @@ from finite_diff.finite_diff import Interpolation, Lagrange
 import numpy as np
 import matplotlib.pyplot as plt
 
-n = 10
-q = 6
+plt.rcParams.update({"font.size": 11})
 
-non_unif = Interpolation(n, q, max_iter=910)
+n = 10
+q = 10
+
+non_unif = Interpolation(n, q, max_iter=10)
 x = np.linspace(-1, 1, n + 1)
 unif = Lagrange(x)
 
@@ -16,12 +18,16 @@ y = np.linspace(-1, 1, 201)
 u = 1 / (1 + 25 * non_unif.inter.x**2)
 u = np.array([np.dot(non_unif(k), u) for k in y])
 
-v = 1 / (1 + 25 * x**2)
-v = np.array([np.sum([unif(k, i) * v[i] for i in range(n + 1)]) for k in y])
+v0 = 1 / (1 + 25 * x**2)
+v = np.array([np.sum([unif(k, i) * v0[i] for i in range(n + 1)]) for k in y])
 
-plt.plot(y, u)
-plt.plot(y, v)
-plt.plot(y, 1 / (1 + 25 * y**2), "--")
-plt.xlabel("$x$")
-plt.ylabel("$f(x)$")
+i = np.array([np.sum([unif(k, i) * v0[i] for i in range(n + 1)]) for k in x])
+
+# plt.plot(y, u, label="Hermanns-Hernández Spacing")
+plt.plot(y, v, "k--", label="Lagrange Interpolation")
+plt.plot(y, 1 / (1 + 25 * y**2), "k:", label="Runge Function")
+plt.plot(x, i, "k.", label="Interpolation Points", markersize=10)
+plt.xlabel("$x$", fontsize=12)
+plt.ylabel("$f(x)$", fontsize=12)
+plt.legend()
 plt.show()
